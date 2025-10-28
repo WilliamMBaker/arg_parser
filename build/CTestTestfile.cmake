58e789716a60a2f7297a53aa53749b897305af35
WilliamMBaker <willbee8888@gmail.com>
2025-10-28 18:57:35 +0000

    Add CMake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Willi/Documents/arg_parser
+# Build directory: C:/Users/Willi/Documents/arg_parser/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
